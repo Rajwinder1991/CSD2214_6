f349571a9aaf48a4e4381b65b53e42134c83ddbb
Rajwinder1991 <43039694+Rajwinder1991@users.noreply.github.com>
2019-01-14 17:03:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//Name:Rajwinder Kaur
+//Date: January 14,2019
+//Student ID:C0743557
+//Course:CSD 2214 Section 6
+// JavaScript source code
+alert("yeah!!! I am a great JavaScript Programmer!!")
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const mintuesSpan = clock.querySelector('.mintues');
+const secondsSpan = clock.querySelector('.seconds');
+
